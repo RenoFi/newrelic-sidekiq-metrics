74ed8b760e5916a8ac32d6ecd7bb539166d480c9
Krzysztof Knapik <knapo@knapo.net>
2019-11-14 10:01:53 +0100

    change spec to use less stubbing and ensure that code in perform is executed

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,20 +1,24 @@
+class HelloService
+  def self.call(arg)
+  end
+end
+
 class HelloWorker
   include Sidekiq::Worker
 
   def perform(arg)
-    arg
+    HelloService.call(arg)
   end
 end
 
 RSpec.describe 'Sidekiq Integration', type: :integration do
-  describe 'HelloWorker.perform_async(1)' do
-    subject { HelloWorker.perform_async(1) }
+  describe 'HelloWorker.perform_async' do
+    let(:arg) { SecureRandom.hex }
 
-    it 'HelloWorker receives :new, :perform with 1' do
-      instance = HelloWorker.new
-      expect(HelloWorker).to receive(:new) { instance }
-      expect(instance).to receive(:perform).with(1)
-      subject
+    it 'executes instance with given arg' do
+      expect(HelloService).to receive(:call).with(arg)
+
+      HelloWorker.perform_async(arg)
     end
   end
 end
