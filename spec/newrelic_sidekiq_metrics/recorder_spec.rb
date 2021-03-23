ae9cd231d73f40d81e076d5e2f339fcf3e369eab
Krzysztof Knapik <knapo@knapo.net>
2021-03-23 11:50:17 +0100

    Skip connecting redis in sidekiq inline mode (#98)

--- spec/newrelic_sidekiq_metrics/recorder_spec.rb
+++ spec/newrelic_sidekiq_metrics/recorder_spec.rb
@@ -1,14 +1,37 @@
 RSpec.describe NewrelicSidekiqMetrics::Recorder do
   describe '#call' do
-    before do
-      NewrelicSidekiqMetrics.use(:enqueued, :workers_size)
+    context 'with inline sidekiq mode' do
+      around do |example|
+        Sidekiq::Testing.inline! do
+          example.run
+        end
+      end
+
+      specify do
+        expect(NewrelicSidekiqMetrics.inline_sidekiq?).to eq(true)
+
+        NewrelicSidekiqMetrics.use(:enqueued, :workers_size)
+
+        expect(Sidekiq::Stats).not_to receive(:new)
+        expect(NewRelic::Agent).to receive(:record_metric).with('Custom/Sidekiq/EnqueuedSize', 0).and_call_original
+        expect(NewRelic::Agent).to receive(:record_metric).with('Custom/Sidekiq/WorkersSize', 0).and_call_original
+
+        subject.call
+      end
     end
 
-    it do
-      expect(NewRelic::Agent).to receive(:record_metric).with('Custom/Sidekiq/EnqueuedSize', 0).and_call_original
-      expect(NewRelic::Agent).to receive(:record_metric).with('Custom/Sidekiq/WorkersSize', 0).and_call_original
+    context 'with default sidekiq mode' do
+      specify do
+        expect(NewrelicSidekiqMetrics.inline_sidekiq?).to eq(false)
 
-      subject.call
+        NewrelicSidekiqMetrics.use(:enqueued, :workers_size)
+
+        expect(Sidekiq::Stats).to receive(:new).and_call_original
+        expect(NewRelic::Agent).to receive(:record_metric).with('Custom/Sidekiq/EnqueuedSize', 0).and_call_original
+        expect(NewRelic::Agent).to receive(:record_metric).with('Custom/Sidekiq/WorkersSize', 0).and_call_original
+
+        subject.call
+      end
     end
   end
 end
