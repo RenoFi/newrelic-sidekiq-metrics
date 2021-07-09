6144884755c3f8dd7f8750ef4823ac50b37f6f08
Krzysztof Knapik <knapo@knapo.net>
2021-07-09 10:20:21 +0200

    remove real sidekiq usage

--- spec/newrelic_sidekiq_metrics/recorder_spec.rb
+++ spec/newrelic_sidekiq_metrics/recorder_spec.rb
@@ -21,14 +21,16 @@ RSpec.describe NewrelicSidekiqMetrics::Recorder do
     end
 
     context 'with default sidekiq mode' do
+      let(:stats) { instance_double(Sidekiq::Stats, enqueued: 4, workers_size: 2) }
+
       specify do
         expect(NewrelicSidekiqMetrics.inline_sidekiq?).to eq(false)
 
         NewrelicSidekiqMetrics.use(:enqueued, :workers_size)
 
-        expect(Sidekiq::Stats).to receive(:new).and_call_original
-        expect(NewRelic::Agent).to receive(:record_metric).with('Custom/Sidekiq/EnqueuedSize', 0).and_call_original
-        expect(NewRelic::Agent).to receive(:record_metric).with('Custom/Sidekiq/WorkersSize', 0).and_call_original
+        expect(Sidekiq::Stats).to receive(:new) { stats }
+        expect(NewRelic::Agent).to receive(:record_metric).with('Custom/Sidekiq/EnqueuedSize', 4).and_call_original
+        expect(NewRelic::Agent).to receive(:record_metric).with('Custom/Sidekiq/WorkersSize', 2).and_call_original
 
         subject.call
       end
